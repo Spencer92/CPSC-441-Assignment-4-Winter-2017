b50782b44fa7216b2f6785b37b4ec35c9a81a8f5
Spencer Maslen <smhmaslen@gmail.com>
2017-04-03 15:00:56 -0600

    Got some linkstuff working

--- CPSC 441 Assignment 4/src/router/router/LinkStateReceiver.java
+++ CPSC 441 Assignment 4/src/router/router/LinkStateReceiver.java
@@ -1,13 +1,49 @@
 package router.router;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
 public class LinkStateReceiver implements Runnable
 {
 	Router router;
+	DatagramSocket clientSocket;
 	
-	
+	public LinkStateReceiver(Router router, DatagramSocket clientSocket)
+	{
+		this.router = router;
+		this.clientSocket = clientSocket;
+	}
 	
 	@Override
 	public void run() {
+		
+		
+		while(!router.isEnd())
+		{
+			byte [] linkStateInfo = new byte[LinkState.MAX_PAYLOAD_SIZE];
+			DatagramPacket receivePacket = new DatagramPacket(linkStateInfo,linkStateInfo.length);	
+			
+			try {
+				clientSocket.receive(receivePacket);
+				router.processUpDateDS(receivePacket);
+			} catch (IOException e) {
+				
+				if(router.isEnd())
+				{
+					break;
+				}
+				else
+				{
+					e.printStackTrace();
+				}
+			}
+			
+			
+			
+		}
+		
+		
 		// TODO Auto-generated method stub
 		
 	}
